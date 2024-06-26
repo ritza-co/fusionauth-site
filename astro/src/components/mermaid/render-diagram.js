63a0032b16da287d1676dbe1f2806c28cd00ea94
RichardJECooke <RichardJECooke@pm.me>
2024-06-26 13:33:12 +0200

    Upgrade Mermaid to version 10. Handles text in flowchart arrows correctly now.

--- astro/src/components/mermaid/render-diagram.js
+++ astro/src/components/mermaid/render-diagram.js
@@ -14,24 +14,29 @@ const config = {
 export default async function renderDiagram({ code, id, nostyle=true }) {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
+  await page.setContent('<!DOCTYPE html><html><head></head><body></body></html>');
 
   const content = await fs.readFile(
     path.join(process.cwd(), 'node_modules/mermaid/dist/mermaid.js'),
     'utf8',
   );
 
+  page.on('console', msg => console.log('Mermaid render error in render-diagram.js:', msg.text()));
+
   await page.addScriptTag({ content });
 
   const result = await page.evaluate(
-    (configB, codeB, id) => {
+    async (configB, codeB, id) => {
       // FIXME: `window.mermaid` global browser stubbing
       window.mermaid.initialize(configB);
 
       try {
         // Render the mermaid diagram
-        const svgCode = window.mermaid.mermaidAPI.render(id, codeB);
-        return { status: 'success', svgCode };
+        const svgCode = await window.mermaid.mermaidAPI.render(id, codeB);
+        return Promise.resolve({ status: 'success', svgCode: svgCode.svg });
       } catch (error) {
+        console.log(error);
+        console.log(error.message);
         return { status: 'error', error, message: error.message };
       }
     },
