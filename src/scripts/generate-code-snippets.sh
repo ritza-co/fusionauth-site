15d97ae930b880ddb58d121a2f69989288d9b483
RichardJECooke <RichardJECooke@pm.me>
2026-07-15 12:53:45 +0200

    remove tmp and cache files from bluehawk

--- src/scripts/generate-code-snippets.sh
+++ src/scripts/generate-code-snippets.sh
@@ -25,6 +25,8 @@ for repo in src/code-example-repositories/*/; do
 		--ignore 'LICENSE' \
 		--ignore 'SECURITY.md' \
 		--ignore '*.log' \
+		--ignore 'tmp' \
+		--ignore '*.cache' \
 		2>&1) || status=$?
 	printf '%s\n' "$out" | grep -v 'parsed file' | grep -v 'found binary file' || true
 	if [ $status -ne 0 ] || printf '%s\n' "$out" | grep -q 'bluehawk errors'; then
