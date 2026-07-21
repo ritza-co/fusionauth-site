f8d505228159debaa0e58756a1be692a12474ad7
RichardJECooke <RichardJECooke@pm.me>
2026-07-21 14:15:24 +0200

    add timeout for FA

--- astro/src/code-example-repositories/fusionauth-quickstart-javascript-nextjs-web/tests/integration.spec.js
+++ astro/src/code-example-repositories/fusionauth-quickstart-javascript-nextjs-web/tests/integration.spec.js
@@ -4,6 +4,7 @@ test('FusionAuth admin login', async ({ page }) => {
   await page.goto('http://localhost:9011/admin/');
   await page.waitForLoadState('networkidle');
 
+  await expect(page.getByPlaceholder('Login')).toBeVisible({ timeout: 60000 });
   await page.getByPlaceholder('Login').fill('admin@example.com');
   await page.getByPlaceholder('Password').fill('password');
   await page.getByRole('button', { name: 'Submit' }).click();
