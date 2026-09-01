8a22aee27f01f267607cfa35430e8e32247c0e2f
RichardJECooke <RichardJECooke@pm.me>
2026-09-01 11:21:52 +0200

    Add repo to localcode from https://github.com/FusionAuth/fusionauth-quickstart-php-laravel-api

---
+++ astro/localcode/quickstart-php-laravel-api/complete-application/app/FusionAuth/Providers/FusionAuthEloquentUserProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FusionAuth\Providers;
+
+use App\Models\User;
+use Illuminate\Auth\EloquentUserProvider;
+use Tymon\JWTAuth\Payload;
+
+class FusionAuthEloquentUserProvider extends EloquentUserProvider
+{
+
+    /**
+     * Returns a user from the provided payload
+     *
+     * @param \Tymon\JWTAuth\Payload $payload
+     *
+     * @return \App\Models\User
+     */
+    public function createModelFromPayload(Payload $payload): User
+    {
+        $email = $payload->get('email');
+        /** @var \App\Models\User $model */
+        $model = $this->createModel()
+            ->setAttribute('id', $payload->get('sub'));
+        return $model;
+    }
+}
+
