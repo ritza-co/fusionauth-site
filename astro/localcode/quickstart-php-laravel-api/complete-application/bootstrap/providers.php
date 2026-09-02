ce24957831e3ca5c400e4722ae4f162ac14ab894
RichardJECooke <RichardJECooke@pm.me>
2026-09-02 13:25:14 +0200

    remove unnessary tag

--- astro/localcode/quickstart-php-laravel-api/complete-application/bootstrap/providers.php
+++ astro/localcode/quickstart-php-laravel-api/complete-application/bootstrap/providers.php
@@ -2,9 +2,7 @@
 
 use App\Providers\AppServiceProvider;
 
-// :snippet-start: providers
 return [
     AppServiceProvider::class,
     App\FusionAuth\Providers\FusionAuthServiceProvider::class,
 ];
-// :snippet-end:
