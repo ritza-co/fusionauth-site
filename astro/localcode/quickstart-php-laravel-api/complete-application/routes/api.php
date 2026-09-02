b8b342faac110bbfc84f7427c3b6cb075342d62c
RichardJECooke <RichardJECooke@pm.me>
2026-09-02 14:34:31 +0200

    use jwt auth not sanctum

--- astro/localcode/quickstart-php-laravel-api/complete-application/routes/api.php
+++ astro/localcode/quickstart-php-laravel-api/complete-application/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-$middleware = Route::middleware('auth:sanctum');
+$middleware = Route::middleware('auth:web');
 $middleware->get('/user', function (Request $request) {
     return $request->user();
 });
