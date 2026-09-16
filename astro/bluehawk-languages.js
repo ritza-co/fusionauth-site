62f599a67dabfa5847ccf2a0c48afeb21b7d1fd5
RichardJECooke <RichardJECooke@pm.me>
2026-09-16 16:21:50 +0200

    fixing tag errors

--- astro/bluehawk-languages.js
+++ astro/bluehawk-languages.js
@@ -1,6 +1,9 @@
 export function register({ bluehawk }) {
   // Bluehawk 1.6.0 only includes a small built-in language set; these cover
   // common example-file extensions that otherwise warn or fail during snip/copy.
+  bluehawk.addLanguage(["mts", "cts"], {
+    languageId: "typescript",
+  });
   bluehawk.addLanguage(["yml"], {
     languageId: "yaml",
     lineComments: [/#/],
